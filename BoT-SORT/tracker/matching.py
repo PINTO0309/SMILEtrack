7d9a29a46b080eba4cb40afb9a8ace7e8a9471d3
pinto0309 <rmsdh122@yahoo.co.jp>
2024-01-01 11:46:48 +0000

    update

--- BoT-SORT/tracker/matching.py
+++ BoT-SORT/tracker/matching.py
@@ -197,33 +197,6 @@ class STrack(BaseTrack):
     def __repr__(self):
         return 'OT_{}_({}-{})'.format(self.track_id, self.start_frame, self.end_frame)
 
-def merge_matches(m1, m2, shape):
-    O,P,Q = shape
-    m1 = np.asarray(m1)
-    m2 = np.asarray(m2)
-
-    M1 = scipy.sparse.coo_matrix((np.ones(len(m1)), (m1[:, 0], m1[:, 1])), shape=(O, P))
-    M2 = scipy.sparse.coo_matrix((np.ones(len(m2)), (m2[:, 0], m2[:, 1])), shape=(P, Q))
-
-    mask = M1*M2
-    match = mask.nonzero()
-    match = list(zip(match[0], match[1]))
-    unmatched_O = tuple(set(range(O)) - set([i for i, j in match]))
-    unmatched_Q = tuple(set(range(Q)) - set([j for i, j in match]))
-
-    return match, unmatched_O, unmatched_Q
-
-
-def _indices_to_matches(cost_matrix, indices, thresh):
-    matched_cost = cost_matrix[tuple(zip(*indices))]
-    matched_mask = (matched_cost <= thresh)
-
-    matches = indices[matched_mask]
-    unmatched_a = tuple(set(range(cost_matrix.shape[0])) - set(matches[:, 0]))
-    unmatched_b = tuple(set(range(cost_matrix.shape[1])) - set(matches[:, 1]))
-
-    return matches, unmatched_a, unmatched_b
-
 
 def linear_assignment(cost_matrix: np.ndarray, thresh: float):
     if cost_matrix.size == 0:
@@ -259,20 +232,6 @@ def ious(atlbrs: List[np.ndarray], btlbrs: List[np.ndarray]):
     return ious
 
 
-def tlbr_expand(tlbr, scale=1.2):
-    w = tlbr[2] - tlbr[0]
-    h = tlbr[3] - tlbr[1]
-
-    half_scale = 0.5 * scale
-
-    tlbr[0] -= half_scale * w
-    tlbr[1] -= half_scale * h
-    tlbr[2] += half_scale * w
-    tlbr[3] += half_scale * h
-
-    return tlbr
-
-
 def iou_distance(atracks: List[STrack], btracks: List[STrack]):
     """
     Compute cost based on IoU
@@ -294,28 +253,7 @@ def iou_distance(atracks: List[STrack], btracks: List[STrack]):
     return cost_matrix
 
 
-def v_iou_distance(atracks: List[STrack], btracks: List[STrack]):
-    """
-    Compute cost based on IoU
-    :type atracks: list[STrack]
-    :type btracks: list[STrack]
-
-    :rtype cost_matrix np.ndarray
-    """
-
-    if (len(atracks)>0 and isinstance(atracks[0], np.ndarray)) or (len(btracks) > 0 and isinstance(btracks[0], np.ndarray)):
-        atlbrs = atracks
-        btlbrs = btracks
-    else:
-        atlbrs = [track.tlwh_to_tlbr(track.pred_bbox) for track in atracks]
-        btlbrs = [track.tlwh_to_tlbr(track.pred_bbox) for track in btracks]
-    _ious = ious(atlbrs, btlbrs)
-    cost_matrix = 1 - _ious
-
-    return cost_matrix
-
-
-def embedding_distance(tracks, detections, metric='cosine'):
+def embedding_distance(tracks: List[STrack], detections: List[STrack], metric='cosine'):
     """
     :param tracks: list[STrack]
     :param detections: list[BaseTrack]
@@ -333,12 +271,11 @@ def embedding_distance(tracks, detections, metric='cosine'):
     return cost_matrix
 
 
-def gate_cost_matrix(kf, cost_matrix, tracks, detections, only_position=False):
+def gate_cost_matrix(kf: KalmanFilter, cost_matrix: np.ndarray, tracks: List[STrack], detections: List[STrack], only_position=False):
     if cost_matrix.size == 0:
         return cost_matrix
     gating_dim = 2 if only_position else 4
     gating_threshold = KalmanFilter.chi2inv95[gating_dim]
-    # measurements = np.asarray([det.to_xyah() for det in detections])
     measurements = np.asarray([det.to_xywh() for det in detections])
     for row, track in enumerate(tracks):
         gating_distance = kf.gating_distance(
@@ -347,12 +284,11 @@ def gate_cost_matrix(kf, cost_matrix, tracks, detections, only_position=False):
     return cost_matrix
 
 
-def fuse_motion(kf, cost_matrix, tracks, detections, only_position=False, lambda_=0.98):
+def fuse_motion(kf: KalmanFilter, cost_matrix: np.ndarray, tracks: List[STrack], detections: List[STrack], only_position=False, lambda_=0.98):
     if cost_matrix.size == 0:
         return cost_matrix
     gating_dim = 2 if only_position else 4
     gating_threshold = KalmanFilter.chi2inv95[gating_dim]
-    # measurements = np.asarray([det.to_xyah() for det in detections])
     measurements = np.asarray([det.to_xywh() for det in detections])
     for row, track in enumerate(tracks):
         gating_distance = kf.gating_distance(
@@ -362,7 +298,7 @@ def fuse_motion(kf, cost_matrix, tracks, detections, only_position=False, lambda
     return cost_matrix
 
 
-def fuse_iou(cost_matrix, tracks, detections):
+def fuse_iou(cost_matrix: np.ndarray, tracks: List[STrack], detections: List[STrack]):
     if cost_matrix.size == 0:
         return cost_matrix
     reid_sim = 1 - cost_matrix
@@ -371,12 +307,11 @@ def fuse_iou(cost_matrix, tracks, detections):
     fuse_sim = reid_sim * (1 + iou_sim) / 2
     det_scores = np.array([det.score for det in detections])
     det_scores = np.expand_dims(det_scores, axis=0).repeat(cost_matrix.shape[0], axis=0)
-    #fuse_sim = fuse_sim * (1 + det_scores) / 2
     fuse_cost = 1 - fuse_sim
     return fuse_cost
 
 
-def fuse_score(cost_matrix, detections):
+def fuse_score(cost_matrix: np.ndarray, detections: List[STrack]):
     if cost_matrix.size == 0:
         return cost_matrix
     iou_sim = 1 - cost_matrix
@@ -386,18 +321,15 @@ def fuse_score(cost_matrix, detections):
     fuse_cost = 1 - fuse_sim
     return fuse_cost
 
-def gate(cost_matrix, emb_cost):
+def gate(cost_matrix: np.ndarray, emb_cost: np.ndarray):
     """
     :param tracks: list[STrack]
     :param detections: list[BaseTrack]
     :param metric:
     :return: cost_matrix np.ndarray
     """
-
     if cost_matrix.size == 0:
         return cost_matrix
-
     index = emb_cost > 0.3
     cost_matrix[index] = 1
-
     return cost_matrix
